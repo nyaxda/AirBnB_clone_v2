5a3db3ea5fd1961a065a6764bf95941cb3910f1d
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-17 05:14:48 -0700

    Delete print statements

--- models/state.py
+++ models/state.py
@@ -6,7 +6,3 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        print('kwargs: {}'.format(kwargs))
-        print(self.__dict__)
