af8a39f050d044834f9db21784827b34e2451827
NyaxDa <nyaxda@gmail.com>
2024-05-03 13:02:25 -0700

    Added File 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,6 +6,7 @@ from fabric.api import env
 from fabric.api import local
 from fabric.api import put
 from fabric.api import run
+from fabric.api import sudo
 
 env.hosts = ["100.25.170.102", "100.24.236.169"]
 
@@ -40,23 +41,23 @@ def do_deploy(archive_path):
     if run("rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
+    if sudo("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+    if sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if sudo("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
+    if sudo("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
+    if sudo("rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if sudo("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    if sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
