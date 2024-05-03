829cd601b8908c4223ab9dafdef4634c77d7f03a
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-03 12:12:13 -0700

    Refactor do_deploy function to move web_static files in 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Fabfile
-from fabric.api import env, run, put, settings
+from fabric.api import env, run, put
 import os.path
 
 env.hosts = ['100.25.170.102',
@@ -12,27 +12,32 @@ def do_deploy(archive_path):
     """Deploy an archive to the web servers."""
     if os.path.isfile(archive_path) is False:
         return False
-    with settings(warn_only=False):
-        result = put(archive_path, "/tmp/")
-        if result.failed:
-            return False
-        archive_name = os.path.basename(archive_path)
-        name, ext = os.path.splitext(archive_name)
-        result = run("mkdir -p /data/web_static/releases/{}/".format(name))
-        if result.failed:
-            return False
-        result = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            archive_name, name))
-        if result.failed:
-            return False
-        result = run("rm /tmp/{}".format(archive_name))
-        if result.failed:
-            return False
-        result = run("rm -rf /data/web_static/current")
-        if result.failed:
-            return False
-        result = run("""ln -s /data/web_static/releases/{}
-            /data/web_static/current""".format(name))
-        if result.failed:
-            return False
-        return True
+    result = put(archive_path, "/tmp/")
+    if result.failed:
+        return False
+    archive_name = os.path.basename(archive_path)
+    name, ext = os.path.splitext(archive_name)
+    result = run("mkdir -p /data/web_static/releases/{}/".format(name))
+    if result.failed:
+        return False
+    result = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+        archive_name, name))
+    if result.failed:
+        return False
+    result = run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+    if result.failed:
+        return False
+    result = run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+    if result.failed:
+        return False
+    result = run("rm /tmp/{}".format(archive_name))
+    if result.failed:
+        return False
+    result = run("rm -rf /data/web_static/current")
+    if result.failed:
+        return False
+    result = run("""ln -s /data/web_static/releases/{}
+        /data/web_static/current""".format(name))
+    if result.failed:
+        return False
+    return True
