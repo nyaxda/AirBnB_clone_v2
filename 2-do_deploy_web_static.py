2d6b9688e32886095243cb3256a0e6e8ae380f16
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-03 12:33:25 -0700

    Refactor do_deploy function to fix file type check in 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,23 +16,21 @@ def do_deploy(archive_path):
     name, ext = os.path.splitext(archive_name)
     if put(archive_path, "/tmp/").failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".format(
-            name)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            archive_name, name)).failed is True:
+        archive_name, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(archive_name)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".format(
-            name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("""ln -s /data/web_static/releases/{}/
-           /data/web_static/current""".format(name)).failed is True:
+    if run("ln -s /data/web_static/releases/{}/ "
+           "/data/web_static/current".format(name)).failed is True:
         return False
     return True
