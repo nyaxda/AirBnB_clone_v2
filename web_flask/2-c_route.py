58e4852f93086db1016e49b682452ff6b2895b68
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-17 07:02:19 -0700

    Added File 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Display C """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0')
