950a4002ae188b08bc7db3cd0ccd78ef04ebdb54
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-17 06:52:42 -0700

    Added File 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display HBNB """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Display HBNB """
+    return "c {}".format(text.replace('_', ' '))
+
+@app.route('/python/<text>', defaults={"text": "is cool"}, strict_slashes=False)
+def python(text):
+    """ Display HBNB """
+    return "python {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0')
