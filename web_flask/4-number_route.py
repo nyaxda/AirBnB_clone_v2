dd402d840bd34526222859f7ebbdc75bbe0647b5
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 05:57:54 -0700

    Refactor number route to handle non-integer inputs gracefully

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,7 +32,7 @@ def python(text="is cool"):
 
 @app.route('/number/<int: n>', strict_slashes=False)
 def number(n):
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 
 if __name__ == '__main__':
