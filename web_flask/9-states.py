bc9d01c8e2704b33fbf48c641ebb47b235527d15
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 12:37:24 -0700

    Added File 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state_id(id):
+    """Displays an HTML page with a state of certain id."""
+    states = storage.all("State")
+    state = None
+    for state_obj in states.values():
+        if state_obj.id == id:
+            state = state_obj
+            break
+    else:
+        return render_template("9-states.html", state=state)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
