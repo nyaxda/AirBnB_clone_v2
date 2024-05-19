f68fd5f2d86abd5aa3c163029eda1a29ddce7757
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 08:09:15 -0700

    pycodestyle

--- models/state.py
+++ models/state.py
@@ -5,11 +5,14 @@ from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",  cascade="all, delete, delete-orphan")
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete, delete-orphan")
 
     @property
     def cities(self):
