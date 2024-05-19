f7471cd68995a60e24d487b5bd63b68567c7600b
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 05:48:48 -0700

    feat: Add number route to Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Display C """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ Display Python """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    if n.isdigit():
+        return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0')
