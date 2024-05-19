5c7fae17ff805d2d0e72134b2138d9c01ea4a6f0
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 06:16:56 -0700

    refactor: Rename number route function to number_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number(n):
+def number_template(n):
     """Display html"""
     return render_template('5-number.html', n=n)
 
