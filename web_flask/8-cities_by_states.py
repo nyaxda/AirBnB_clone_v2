3d91d5e6867c444aec3520c5657037a58e1bcc97
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 10:28:53 -0700

    Correction

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,9 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities():
     """Displays HTML page with states sorted from A to Z"""
-    states = storage.all(State).values()
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    for state in states:
+        state.cities = sorted(state.cities, key=lambda city: city.name)
     return render_template('8-cities_by_states.html',
                            states=states)
 
