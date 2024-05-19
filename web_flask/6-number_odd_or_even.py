8dd81e95537e8d00ea9422e6a3fd40a8d16fc547
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 06:54:24 -0700

    Errorfix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,7 +46,7 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """Display html"""
-    return render_template('6-number_odd_or_even.html', m=n)
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
