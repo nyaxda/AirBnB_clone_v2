b141f74364df3d3bf6da7b781716c0fdb504a4d5
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 10:49:18 -0700

    Refactor variable assignment for states in 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities():
     """Displays HTML page with states sorted from A to Z"""
-    states = sorted(storage.all("State"))
+    states = storage.all("State")
     return render_template('8-cities_by_states.html',
                            states=states)
 
