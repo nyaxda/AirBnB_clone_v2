8a180b73a7e38b52dc2f086aee97f7c8ba540c97
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 10:18:59 -0700

    Errorfix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -11,10 +12,9 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities():
     """Displays HTML page with states sorted from A to Z"""
-    states = storage.all("State")
-    cities = storage.all("City")
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html',
-                           states=states, cities=cities)
+                           states=states)
 
 
 @app.teardown_appcontext
