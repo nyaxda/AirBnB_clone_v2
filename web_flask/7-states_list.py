444f1c9d5a306c3c60c49e1c9d545e0ff356d72a
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-19 09:07:28 -0700

    Refactor variable assignment for states in 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,17 +7,16 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(exc):
-    """Closes the storage on teardown"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states():
     """Displays HTML page with states sorted from A to Z"""
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Closes the storage on teardown"""
+    storage.close()
 
 
 if __name__ == '__main__':
