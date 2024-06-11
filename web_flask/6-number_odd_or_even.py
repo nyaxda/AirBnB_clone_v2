1324564ad4e8ddfb586ccc0710431e1effd02504
Daniel Nyakundi <nyaxda@gmail.com>
2024-06-11 10:18:39 -0700

    chore: Update route in 6-number_odd_or_even.py to "/airbnb-dynamic/number_odd_or_even/"

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-@app.route('/airbnb_dynamic/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/airbnb-dynamic/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """Display html"""
     return render_template('6-number_odd_or_even.html', n=n)
